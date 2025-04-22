fdd3a5ed78ff9b749b31d4ecc1e757b0ad8beed4
Nícolas Grande <nmgrande@hotmail.com>
2025-04-21 23:35:58 -0300

    arrumar filtros

--- BO/autoencoder/autoencoder.py
+++ BO/autoencoder/autoencoder.py
@@ -281,7 +281,7 @@ class Autoencoder(AutoencoderConfiguracao):
 
         return self.encoder
 
-    def criar_decoder_lu(self):
+    def criar_decoder(self):
         """
         Função responsável em criar a segunda parte do autoencoder, o Decoder.
         Ele começa calculando qual é a altura e largura da primeira etapa, para ser possível reconstruir fielmente o decoder,
@@ -294,10 +294,12 @@ class Autoencoder(AutoencoderConfiguracao):
         """
         altura, largura = self.calcular_saida_encoder()
         self.decoder = models.Sequential()
+        reshape = (self.encoder.layers[-2].input.shape[1], self.encoder.layers[-2].input.shape[2],
+                   self.encoder.layers[-2].input.shape[3])
         self.decoder.add(layers.InputLayer(shape=(300,)))
-        self.decoder.add(layers.Dense(units=65536,
-                                      activation='relu'))  # self.encoder.layers[-1].input.shape[1], activation=self.activation))
-        self.decoder.add(layers.Reshape((96, 96, 1)))  # self.encoder.layers[-2].input.shape[1:])))
+        self.decoder.add(layers.Dense(units=reshape[0] * reshape[1] * reshape[2],
+                                      activation='relu'))  # encoder.layers[-1].input.shape[1], activation=activation))
+        self.decoder.add(layers.Reshape(reshape))  # encoder.layers[-2].input.shape[1:])))
 
         self.decoder.add(layers.Conv2DTranspose(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
         self.decoder.add(layers.Conv2DTranspose(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
@@ -321,7 +323,7 @@ class Autoencoder(AutoencoderConfiguracao):
 
         return self.decoder
 
-    def criar_decoder(self):
+    def criar_decoder_e(self):
         """
         Função responsável em criar a segunda parte do autoencoder, o Decoder.
         Ele começa calculando qual é a altura e largura da primeira etapa, para ser possível reconstruir fielmente o decoder,
