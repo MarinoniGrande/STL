8d5de37003573fe9109a4ed561fc5d2ab9ba59ee
Nícolas Marinoni Grande <nicolas.grande@inteliger.com.br>
2025-09-18 19:33:38 -0300

    alterar fastai

--- comando4.sh
+++ comando4.sh
@@ -33,4 +33,4 @@ nohup python3 main.py CROSS9_NAZMUL_SVC_90 classificar > CROSS9_NAZMUL_SVC_90.lo
 nohup python3 main.py CROSS9_YILDRIM_SVC_60 classificar > CROSS9_YILDRIM_SVC_60.log 2>&1
 nohup python3 main.py CROSS9_YILDRIM_SVC_70 classificar > CROSS9_YILDRIM_SVC_70.log 2>&1
 nohup python3 main.py CROSS9_YILDRIM_SVC_80 classificar > CROSS9_YILDRIM_SVC_80.log 2>&1
-nohup python3 main.py CROSS9_YILDRIM_SVC_90 classificar > CROSS9_YILDRIM_SVC_90.log 2>&1
+nohup python3 main.py CROSS9_YILDRIM_SVC_90 classificar > CROSS9_YILDRIM_SVC_90.log 2>&1 &
