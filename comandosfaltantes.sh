ba8b5664853d26589d9b45ae82ce8e063d032f33
Nícolas Marinoni Grande <nicolas.grande@inteliger.com.br>
2025-09-21 10:19:09 -0300

    alterar fastai

--- comandosfaltantes.sh
+++ comandosfaltantes.sh
@@ -9,16 +9,4 @@ nohup python3 main.py CROSS9_NAZMUL_SVC_90 classificar > CROSS9_NAZMUL_SVC_90.lo
 nohup python3 main.py CROSS9_YILDRIM_SVC_60 classificar > CROSS9_YILDRIM_SVC_60.log 2>&1
 nohup python3 main.py CROSS9_YILDRIM_SVC_70 classificar > CROSS9_YILDRIM_SVC_70.log 2>&1
 nohup python3 main.py CROSS9_YILDRIM_SVC_80 classificar > CROSS9_YILDRIM_SVC_80.log 2>&1
-nohup python3 main.py CROSS9_YILDRIM_SVC_90 classificar > CROSS9_YILDRIM_SVC_90.log 2>&1
-nohup python3 main.py CROSS12_CIFAR_SVC_60 classificar > CROSS12_CIFAR_SVC_60.log 2>&1
-nohup python3 main.py CROSS12_CIFAR_SVC_70 classificar > CROSS12_CIFAR_SVC_70.log 2>&1
-nohup python3 main.py CROSS12_CIFAR_SVC_80 classificar > CROSS12_CIFAR_SVC_80.log 2>&1
-nohup python3 main.py CROSS12_CIFAR_SVC_90 classificar > CROSS12_CIFAR_SVC_90.log 2>&1
-nohup python3 main.py CROSS12_NAZMUL_SVC_60 classificar > CROSS12_NAZMUL_SVC_60.log 2>&1
-nohup python3 main.py CROSS12_NAZMUL_SVC_70 classificar > CROSS12_NAZMUL_SVC_70.log 2>&1
-nohup python3 main.py CROSS12_NAZMUL_SVC_80 classificar > CROSS12_NAZMUL_SVC_80.log 2>&1
-nohup python3 main.py CROSS12_NAZMUL_SVC_90 classificar > CROSS12_NAZMUL_SVC_90.log 2>&1
-nohup python3 main.py CROSS12_YILDRIM_SVC_60 classificar > CROSS12_YILDRIM_SVC_60.log 2>&1
-nohup python3 main.py CROSS12_YILDRIM_SVC_70 classificar > CROSS12_YILDRIM_SVC_70.log 2>&1
-nohup python3 main.py CROSS12_YILDRIM_SVC_80 classificar > CROSS12_YILDRIM_SVC_80.log 2>&1
-nohup python3 main.py CROSS12_YILDRIM_SVC_90 classificar > CROSS12_YILDRIM_SVC_90.log 2>&1 &
+nohup python3 main.py CROSS9_YILDRIM_SVC_90 classificar > CROSS9_YILDRIM_SVC_90.log 2>&1 &
