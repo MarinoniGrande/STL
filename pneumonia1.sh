5e0b24b66ea1e6adecd059a3e61af00416cd6865
Nícolas Marinoni Grande <nicolas.grande@inteliger.com.br>
2025-09-21 23:10:27 -0300

    alterar fastai

---
+++ pneumonia1.sh
@@ -0,0 +1,18 @@
+nohup python3 main.py CROSS13_CIFAR_SVC_60 classificar > CROSS13_CIFAR_SVC_60.log 2>&1
+nohup python3 main.py CROSS13_CIFAR_SVC_70 classificar > CROSS13_CIFAR_SVC_70.log 2>&1
+nohup python3 main.py CROSS13_CIFAR_SVC_80 classificar > CROSS13_CIFAR_SVC_80.log 2>&1
+nohup python3 main.py CROSS13_CIFAR_SVC_90 classificar > CROSS13_CIFAR_SVC_90.log 2>&1
+nohup python3 main.py CROSS13_NAZMUL_SVC_60 classificar > CROSS13_NAZMUL_SVC_60.log 2>&1
+nohup python3 main.py CROSS13_NAZMUL_SVC_70 classificar > CROSS13_NAZMUL_SVC_70.log 2>&1
+nohup python3 main.py CROSS13_NAZMUL_SVC_80 classificar > CROSS13_NAZMUL_SVC_80.log 2>&1
+nohup python3 main.py CROSS13_NAZMUL_SVC_90 classificar > CROSS13_NAZMUL_SVC_90.log 2>&1
+nohup python3 main.py CROSS13_YILDRIM_SVC_60 classificar > CROSS13_YILDRIM_SVC_60.log 2>&1
+nohup python3 main.py CROSS13_YILDRIM_SVC_70 classificar > CROSS13_YILDRIM_SVC_70.log 2>&1
+nohup python3 main.py CROSS13_YILDRIM_SVC_80 classificar > CROSS13_YILDRIM_SVC_80.log 2>&1
+nohup python3 main.py CROSS13_YILDRIM_SVC_90 classificar > CROSS13_YILDRIM_SVC_90.log 2>&1
+nohup python3 main.py CROSS14_CIFAR_SVC_60 classificar > CROSS14_CIFAR_SVC_60.log 2>&1
+nohup python3 main.py CROSS14_CIFAR_SVC_70 classificar > CROSS14_CIFAR_SVC_70.log 2>&1
+nohup python3 main.py CROSS14_CIFAR_SVC_80 classificar > CROSS14_CIFAR_SVC_80.log 2>&1
+nohup python3 main.py CROSS14_CIFAR_SVC_90 classificar > CROSS14_CIFAR_SVC_90.log 2>&1
+nohup python3 main.py CROSS14_NAZMUL_SVC_60 classificar > CROSS14_NAZMUL_SVC_60.log 2>&1
+nohup python3 main.py CROSS14_NAZMUL_SVC_70 classificar > CROSS14_NAZMUL_SVC_70.log 2>&1 &
---
+++ pneumonia2.sh
@@ -0,0 +1,18 @@
+nohup python3 main.py CROSS14_NAZMUL_SVC_80 classificar > CROSS14_NAZMUL_SVC_80.log 2>&1
+nohup python3 main.py CROSS14_NAZMUL_SVC_90 classificar > CROSS14_NAZMUL_SVC_90.log 2>&1
+nohup python3 main.py CROSS14_YILDRIM_SVC_60 classificar > CROSS14_YILDRIM_SVC_60.log 2>&1
+nohup python3 main.py CROSS14_YILDRIM_SVC_70 classificar > CROSS14_YILDRIM_SVC_70.log 2>&1
+nohup python3 main.py CROSS14_YILDRIM_SVC_80 classificar > CROSS14_YILDRIM_SVC_80.log 2>&1
+nohup python3 main.py CROSS14_YILDRIM_SVC_90 classificar > CROSS14_YILDRIM_SVC_90.log 2>&1
+nohup python3 main.py CHEST_CIFAR_SVC_60 classificar > CHEST_CIFAR_SVC_60.log 2>&1
+nohup python3 main.py CHEST_CIFAR_SVC_70 classificar > CHEST_CIFAR_SVC_70.log 2>&1
+nohup python3 main.py CHEST_CIFAR_SVC_80 classificar > CHEST_CIFAR_SVC_80.log 2>&1
+nohup python3 main.py CHEST_CIFAR_SVC_90 classificar > CHEST_CIFAR_SVC_90.log 2>&1
+nohup python3 main.py CHEST_NAZMUL_SVC_60 classificar > CHEST_NAZMUL_SVC_60.log 2>&1
+nohup python3 main.py CHEST_NAZMUL_SVC_70 classificar > CHEST_NAZMUL_SVC_70.log 2>&1
+nohup python3 main.py CHEST_NAZMUL_SVC_80 classificar > CHEST_NAZMUL_SVC_80.log 2>&1
+nohup python3 main.py CHEST_NAZMUL_SVC_90 classificar > CHEST_NAZMUL_SVC_90.log 2>&1
+nohup python3 main.py CHEST_YILDRIM_SVC_60 classificar > CHEST_YILDRIM_SVC_60.log 2>&1
+nohup python3 main.py CHEST_YILDRIM_SVC_70 classificar > CHEST_YILDRIM_SVC_70.log 2>&1
+nohup python3 main.py CHEST_YILDRIM_SVC_80 classificar > CHEST_YILDRIM_SVC_80.log 2>&1
+nohup python3 main.py CHEST_YILDRIM_SVC_90 classificar > CHEST_YILDRIM_SVC_90.log 2>&1 &
